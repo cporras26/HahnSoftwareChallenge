365085b45525cb38279b3134b3f4af9faa6c7f8e
Carlos Porras <carlosporrasc26@gmail.com>
2023-04-19 19:30:39 -0600

    Initial Commit

---
+++ TechnicalChallengeBackEnd/TechnicalChallenge.API/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnicalChallenge.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to the Docker World.";
+        }
+    }
+}
